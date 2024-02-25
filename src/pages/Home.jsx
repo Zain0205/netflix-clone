f545a1a7231549ac2c5f8b79a165563463e991f3
Fahmi Zain <fahmizain000@gmail.com>
2024-02-25 13:40:55 +0700

    Create home pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Movie from "../components/Movie";
+import Navbar from "../components/Navbar";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Movie />
+    </>
+  );
+}
+
+export default Home;
