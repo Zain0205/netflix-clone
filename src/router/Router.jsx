6edefa8a426bb4e03e2b87095b1d38773e9b019b
Fahmi Zain <fahmizain000@gmail.com>
2024-02-26 18:52:21 +0700

    Update routes

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
+import LogIn from "../pages/LogIn";
+import SingUp from "../pages/SingUp";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
+  {
+    path: '/login',
+    element: <LogIn />
+  },
+  {
+    path: '/register',
+    element: <SingUp />
+  }
 ]);
