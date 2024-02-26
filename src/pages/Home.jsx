19f93662f67bdcc2d28a1f48fab17e9cee123d89
Fahmi Zain <fahmizain000@gmail.com>
2024-02-26 18:52:49 +0700

    update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import Movie from "../components/Movie";
+import MovieList from "../components/MovieList";
 import Navbar from "../components/Navbar";
+import { request } from "../api/request";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Movie />
+      <MovieList id="1" title="Top Rated" url={request.topRated} />
+      <MovieList id="2" title="Trending" url={request.trending} />
+      <MovieList id="3" title="UpComing" url={request.upComing} />
+      <MovieList id="4" title="Horror" url={request.horror} />
     </>
   );
 }
